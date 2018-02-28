b1d41ea9935515061a5d6ec64be83a7e6048a0ea
hxmanson <mhx_work@163.com>
2018-02-28 12:04:33 +0800

    合并初始文件

---
+++ Example/build/Pods.build/Debug-iphoneos/MHLabelUtil.build/DerivedSources/MHLabelUtil_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char MHLabelUtilVersionString[];
+ extern const double MHLabelUtilVersionNumber;
+
+ const unsigned char MHLabelUtilVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MHLabelUtil  PROJECT:Pods-1" "\n";
+ const double MHLabelUtilVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Example/build/Pods.build/Debug-iphoneos/Pods-MHLabelUtil_Example.build/DerivedSources/Pods_MHLabelUtil_Example_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_MHLabelUtil_ExampleVersionString[];
+ extern const double Pods_MHLabelUtil_ExampleVersionNumber;
+
+ const unsigned char Pods_MHLabelUtil_ExampleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_MHLabelUtil_Example  PROJECT:Pods-1" "\n";
+ const double Pods_MHLabelUtil_ExampleVersionNumber __attribute__ ((used)) = (double)1.;
